bdae40e500d8a6458501e993b6556758f1e3b251
Jr <jira.jiravaraphan@gmail.com>
2016-04-24 02:23:05 +0700

    add chat option

--- Controller.py
+++ Controller.py
@@ -1,6 +1,6 @@
 import connector
 from PySide import QtCore, QtGui
-from views import login, intruction,home
+from views import login, intruction,home,chatOpt
 import sys
 
 
@@ -29,11 +29,18 @@ class Form2(QtGui.QWidget, home.Home):
         self.setWindowOpacity(0.98)
         self.setStyleSheet("background-color:#121317;")
         self.help_button.clicked.connect(self.doHelp)
+        self.chat_button.clicked.connect(self.doChat)
         self.helpWindow=None
+        self.chatopWindow=None
     def doHelp(self):
         if self.helpWindow is None:
             self.helpWindow=Form3()
         self.helpWindow.show()
+    def doChat(self):
+         if self.chatopWindow is None:
+             self.chatopWindow=ChatOptionForm()
+         self.chatopWindow.show()
+
 class Form3(QtGui.QWidget, intruction.Ui_Form):
     def __init__(self, parent=None):
         QtGui.QWidget.__init__(self, parent)
@@ -47,6 +54,12 @@ class ProfileForm(QtGui.QWidget, intruction.Ui_Form):
         self.setupUi(self)
         self.setWindowOpacity(0.95)
 
+class ChatOptionForm(QtGui.QWidget, chatOpt.Ui_Form):
+      def __init__(self, parent=None):
+        QtGui.QWidget.__init__(self, parent)
+        self.setupUi(self)
+        self.setWindowOpacity(0.98)
+        self.setStyleSheet("background-color:#fe3d50;")
 
 
 if __name__ == '__main__':
---
+++ views/chatOpt.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'Downloads/chatOpt.ui'
+#
+# Created: Sun Apr 24 02:02:58 2016
+#      by: pyside-uic 0.2.15 running on PySide 1.2.4
+#
+# WARNING! All changes made in this file will be lost!
+
+from PySide import QtCore, QtGui
+
+class Ui_Form(object):
+    def setupUi(self, Form):
+        Form.setObjectName("Form")
+        Form.resize(371, 192)
+        self.createButton = QtGui.QPushButton(Form)
+        self.createButton.setGeometry(QtCore.QRect(10, 10, 171, 171))
+        font = QtGui.QFont()
+        font.setFamily("Helvetica")
+        font.setPointSize(33)
+        font.setWeight(75)
+        font.setBold(True)
+        self.createButton.setFont(font)
+        self.createButton.setStyleSheet("background-color:#121317;color:white;")
+        self.createButton.setObjectName("createButton")
+        self.joinButton = QtGui.QPushButton(Form)
+        self.joinButton.setStyleSheet("background-color:white;color:#121317;")
+        self.joinButton.setGeometry(QtCore.QRect(190, 10, 171, 171))
+        font = QtGui.QFont()
+        font.setFamily("Helvetica")
+        font.setPointSize(33)
+        font.setWeight(75)
+        font.setBold(True)
+        self.joinButton.setFont(font)
+        self.joinButton.setObjectName("joinButton")
+
+        self.retranslateUi(Form)
+        QtCore.QMetaObject.connectSlotsByName(Form)
+
+    def retranslateUi(self, Form):
+        Form.setWindowTitle(QtGui.QApplication.translate("Form", "Form", None, QtGui.QApplication.UnicodeUTF8))
+        self.createButton.setText(QtGui.QApplication.translate("Form", "CREATE", None, QtGui.QApplication.UnicodeUTF8))
+        self.joinButton.setText(QtGui.QApplication.translate("Form", "JOIN", None, QtGui.QApplication.UnicodeUTF8))
+
