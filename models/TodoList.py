eec4d591dd62de222fd81b9d3bb1e7bee689e29a
Peradon <peradon_charoenchainetr@hotmail.com>
2016-05-20 03:28:36 +0700

    add class

---
+++ models/TodoList.py
@@ -0,0 +1,8 @@
+class TodoList:
+    def __init__(self):
+        self.description = ''
+        self.employee = ''
+
+    def set_TodoList(self, description, employee):
+        self.description = description
+        self.employee = employee
